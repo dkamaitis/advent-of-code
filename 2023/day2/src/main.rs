914a1edf5c93ad55f7cdeb64e94ce4c32605087b
Dovydas Kamaitis <dovkamaitis@gmail.com>
2023-12-06 19:15:52 +0000

    Add unit tests to day2 task1

--- 2023/day2/src/main.rs
+++ 2023/day2/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs;
-mod process_input;
+mod process_task1_input;
 
 fn read_file(file_path: &str) -> Result<String, std::io::Error> {
     return fs::read_to_string(file_path);
@@ -13,6 +13,6 @@ fn main() {
         _ => panic!("Expected exactly one argument: input file path"),
     };
     let file_text = read_file(file_path).expect("Input file should be available in this project");
-    let id_sum = process_input::process_text(&file_text);
+    let id_sum = process_task1_input::process_text(&file_text);
     println!("{}", id_sum);
 }
--- 2023/day2/src/process_task1_input.rs
+++ 2023/day2/src/process_task1_input.rs
@@ -16,11 +16,11 @@ pub fn find_possible_ids(text: &str) -> Option<u32> {
     let game_id: Option<u32> = initial_split?.0.replace("Game ", "").parse::<u32>().ok();
 
     let flat_game_sets = split_game_info(initial_split?.1);
-    println!("{}: {:?}", &game_id?, &flat_game_sets);
+
     let any_larger_than_possible = flat_game_sets
         .iter()
         .any(|x| &x.0.parse::<u32>().unwrap() > &possible_games.get(x.1).unwrap());
-    println!("{}", &any_larger_than_possible);
+
     if !any_larger_than_possible {
         return game_id;
     } else {
@@ -46,9 +46,10 @@ pub fn split_game_info(game_info: &str) -> Vec<(&str, &str)> {
         .collect();
 }
 
+
 #[cfg(test)]
 mod tests {
-    use super::process_text;
+    use super::{find_possible_ids, process_text, split_game_info};
 
     #[test]
     fn finds_correct_sum() {
@@ -60,5 +61,78 @@ mod tests {
             \nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
         );
         assert_eq!(process_text(&example_input), 8);
+    }
+
+    #[test]
+    fn finds_correct_ids() {
+        let test_cases: Vec<(&str, Option<u32>)> = vec![
+            (
+                "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+                Some(1),
+            ),
+            (
+                "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                Some(2),
+            ),
+            (
+                "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+                None,
+            ),
+            (
+                "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+                None,
+            ),
+            (
+                "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+                Some(5),
+            ),
+        ];
+        for (input, expected) in test_cases {
+            assert_eq!(find_possible_ids(input), expected);
+        }
+    }
+
+    #[test]
+    fn splits_game_info_correctly() {
+        let test_cases: Vec<(&str, Vec<(&str, &str)>)> = vec![
+            (
+                "1 red, 4 green; 6 red, 1 green; 10 red; 1 blue, 2 green; 4 green, 3 red; \
+                 1 blue, 8 red",
+                vec![
+                    ("1", "red"),
+                    ("4", "green"),
+                    ("6", "red"),
+                    ("1", "green"),
+                    ("10", "red"),
+                    ("1", "blue"),
+                    ("2", "green"),
+                    ("4", "green"),
+                    ("3", "red"),
+                    ("1", "blue"),
+                    ("8", "red"),
+                ],
+            ),
+            (
+                "9 blue, 13 green, 1 red; 10 green, 4 blue, 4 red; 3 red, 4 blue, 14 green; \
+                 13 blue, 1 red, 12 green",
+                vec![
+                    ("9", "blue"),
+                    ("13", "green"),
+                    ("1", "red"),
+                    ("10", "green"),
+                    ("4", "blue"),
+                    ("4", "red"),
+                    ("3", "red"),
+                    ("4", "blue"),
+                    ("14", "green"),
+                    ("13", "blue"),
+                    ("1", "red"),
+                    ("12", "green"),
+                ],
+            ),
+        ];
+        for (input, expected) in test_cases {
+            assert_eq!(split_game_info(input), expected);
+        }
     }
 }
