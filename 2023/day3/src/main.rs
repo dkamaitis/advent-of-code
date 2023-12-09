23acef8ddf9560855268d6a542432531822ed14d
Dovydas Kamaitis <dovkamaitis@gmail.com>
2023-12-09 17:11:34 +0000

    Update char parsing to use .parse

--- 2023/day3/src/main.rs
+++ 2023/day3/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::fs;
 mod process_task1_input;
-// mod process_task2_input;
+mod process_task2_input;
 
 fn read_file(file_path: &str) -> Result<String, std::io::Error> {
     return fs::read_to_string(file_path);
@@ -14,9 +14,9 @@ fn main() {
         _ => panic!("Expected exactly one argument: input file path"),
     };
     let file_text = read_file(file_path).expect("Input file should be available in this project");
-    let id_sum = process_task1_input::process_text(&file_text);
-    println!("{}", id_sum);
+    // let id_sum = process_task1_input::process_text(&file_text);
+    // println!("{}", id_sum);
 
-    // let cube_power_sum = process_task2_input::process_text(&file_text);
-    // println!("{}", cube_power_sum);
+    let cube_power_sum = process_task2_input::process_text(&file_text);
+    println!("{}", cube_power_sum);
 }
